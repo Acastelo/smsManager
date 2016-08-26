132a99be54899f7b6a261e0156924998a35da459
Maikon Igor <maikonigor@gmail.com>
2016-08-25 22:04:49 -0300

    commit before merge

--- app/pages/contatos/contatos.ts
+++ app/pages/contatos/contatos.ts
@@ -9,24 +9,14 @@ import { Contacts } from 'ionic-native';
 })
 export class Contatos {
 
-  contacts: Array<{displayName: string,  icon: string}>;
+  contacts: Contacts[];
+  contactsToShow: Contacts[];
 
   constructor(public navCtrl: NavController) {
 
-	  this.contacts = [
-	      // { displayName: 'Fulano', icon: 'img/girl_1.png'},
-	      // { displayName: 'Cicrano', icon: 'img/girl_2.png'},
-	      // { displayName: 'Beltrano', icon: 'img/girl_3.png'},
-	      // { displayName: 'Don Vandin', icon: 'img/girl_1.png'},
-	      // { displayName: 'Fulano', icon: 'img/girl_1.png'},
-	      // { displayName: 'Cicrano', icon: 'img/girl_2.png'},
-	      // { displayName: 'Beltrano', icon: 'img/girl_3.png'},
-	      // { displayName: 'Don Vandin', icon: 'img/girl_1.png'},
-	      //  { displayName: 'Fulano', icon: 'img/girl_1.png'},
-	      // { displayName: 'Cicrano', icon: 'img/girl_2.png'},
-	      // { displayName: 'Beltrano', icon: 'img/girl_3.png'},
-	      // { displayName: 'Don Vandin', icon: 'img/girl_1.png'}
-	    ];
+	  this.contacts = [];
+	  this.contactsToShow = [];
+
 	    var opts = {
 	      hasPhoneNumber: true,
 	      multiple: true,
@@ -35,9 +25,26 @@ export class Contatos {
 
       Contacts.find(opts.fields, opts).then((contacts) => {
       	   this.contacts = contacts;
+      	   this.initializeList();
       	   //alert(contacts[0].phoneNumber);
-          alert(JSON.stringify(contacts[1]));
+          //alert(JSON.stringify(contacts[1]));
       });
   }
-  
+
+  initializeList(){
+  	this.contactsToShow = this.contacts;
+  }
+
+  onInput(ev: any){
+
+  	let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+    	this.contactsToShow = this.contactsToShow.filter((item) => {
+        return (item['displayName'].toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+   
 }
