a02c3f8751d00e8fe262907c8237f4222b36338a
Luiz Alexandre Castelo Branco Gomes <funny.alexandre@gmail.com>
2016-09-05 00:55:58 -0300

    Consulta e leitura no banco SQLite

--- app/pages/mensagens/mensagens.ts
+++ app/pages/mensagens/mensagens.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Database } from '../../util/database';
+
 @Component({
   templateUrl: 'build/pages/mensagens/mensagens.html'
 })
 
 export class Mensagem {
   labelEnviar:String;
+  dataAtual= new Date().toISOString();
+  banco = new Database();
+
+  mensagemData = {
+    mensagem: '',
+    dataAgendamento: Date
+  };
 
   mudarBotao(){
     return this.labelEnviar="Agendar"
   }
+
+  salvar(mensagemData){
+    let sql = 'INSERT INTO tb_mensagem(mensagem, dataAgend) values (?,?)';
+
+    this.banco.adicionar(sql, [mensagemData.mensagem, mensagemData.dataAgendamento])
+  }
+
   constructor(public navCtrl: NavController) {
     this.labelEnviar= "Enviar";
   }
--- app/util/database.ts
+++ app/util/database.ts
@@ -1,15 +1,37 @@
 import { SQLite } from 'ionic-native';
 
-let db = new SQLite();
-db.openDatabase({
-  name: 'smsManager.db',
-  location: 'default' // the location field is required
-}).then(() => {
-  db.executeSql('create table danceMoves(name VARCHAR(32))', {}).then(() => {
+export class Database {
+  db = new SQLite();
 
-  }, (err) => {
-    console.error('Unable to execute sql: ', err);
-  });
-}, (err) => {
-  console.error('Unable to open database: ', err);
-});
+  constructor(){
+    this.db.openDatabase({
+      name: 'smsManager.db',
+      location: 'default' // the location field is required
+    }).then(() => {
+      this.db.executeSql('CREATE TABLE tb_mensagem(id INTEGER PRIMARY KEY AUTOINCREMENT,' +
+      ' mensagem TEXT, dataAgend TEXT)', {}).then((retorno) => {
+        console.log("Tabela Criada: ", retorno);
+      }, (err) => {
+        console.error('Unable to execute sql: ', err);
+      });
+    }, (err) => {
+      console.error('Unable to open database: ', err);
+    });
+  }
+
+  adicionar(sql: string, params: Array<Object>[]){
+    this.db.executeSql(sql, params).then((retorno)=>{
+      console.log("Dados inseridos ", + JSON.stringify(retorno));
+    }, (error) =>{
+      console.log("Erro " + JSON.stringify(error.err));
+    });
+  }
+
+  pesquisarTodos(sql:string){
+    this.db.executeSql(sql, []).then((retorno) => {
+      console.log("Pesquisa realizada ", + JSON.stringify(retorno));
+    }, (error) => {
+      console.log("ERROR: " + JSON.stringify(error));
+    });
+  }
+}
